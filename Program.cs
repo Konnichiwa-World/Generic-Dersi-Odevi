270990f212833cda3793c629387597840c47efd1
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-04-28 21:14:09 +0300

    Küp hacimleri ve Daire alanları farkını bulan uygulama
    iki adet açı girince önce küplerin hacimlerini farkını, sonra diğer 2 girdi ile de daire alanları farkını bulan uygulama, generic içermektedir.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generickoleksiyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>alanListesi = new List<int>();
+
+            System.Console.WriteLine("2 adet uzunluk gir ve oluşacak küp alanlarinin farkini gor:");
+            Int32 x =Int32.Parse(Console.ReadLine());
+            Int32 y =Int32.Parse(Console.ReadLine());
+            int a= x*x*x;
+            alanListesi.Add(a);
+            int b= y*y*y;
+            alanListesi.Add(b);
+            foreach(var alan in alanListesi)
+            {System.Console.WriteLine(Math.Abs(alanListesi[0]-alanListesi[1])); }
+
+            List<int>cemberListesi = new List<int>();
+
+            System.Console.WriteLine("2 adet uzunluk gir ve oluşacak daire alanlarinin farkini gor:");
+            Int32 c =Int32.Parse(Console.ReadLine());
+            Int32 d =Int32.Parse(Console.ReadLine());
+            int e= c*c;
+            cemberListesi.Add(e);
+            int f= d*d;
+            cemberListesi.Add(d);
+            foreach(var cember in cemberListesi){System.Console.WriteLine(Math.Abs((e-f)*Math.PI));}
+        }
+    }
+}
